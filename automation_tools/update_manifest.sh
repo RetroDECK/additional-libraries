2417f4f327705d64ca81e35e2f1d2564c7df30c3
XargonWan <XargonWan@gmail.com>
2024-07-25 22:54:58 +0900

    Workflow fixes

--- automation_tools/update_manifest.sh
+++ automation_tools/update_manifest.sh
@@ -4,11 +4,11 @@ git clone https://github.com/XargonWan/RetroDECK --depth=1 RetroDECK
 
 # Creating module manifest
 manifest_header="manifest-header.yml"
-manifest_modules="manifest_module.yml"
+manifest_modules="manifest_modules.yml"
 full_manifest="net.retrodeck.module.yml"
 command="module" 
 
-sed -n '/command/q;p' RetroDECK/net.retrodeck.retrodeck.yml > "$manifest_header"  TEMPORARY DISABLED TO TRY A BUILD WITH ANOTHER RUNTIME
+sed -n '/command/q;p' RetroDECK/net.retrodeck.retrodeck.yml > "$manifest_header"
 echo -e "command: $command\n" >> "$manifest_header"
 sed -i '/^[[:space:]]*#/d' "$manifest_header"
 sed -i 's/[[:space:]]*#.*$//' "$manifest_header"
